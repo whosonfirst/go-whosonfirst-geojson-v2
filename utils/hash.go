d65b4ad9560a172789d1438824129b0ed30d5afc
thisisaaroland <aaron@mapzen.com>
2017-08-01 10:38:21 -0400

    resolve import loop

--- utils/hash.go
+++ utils/hash.go
@@ -1,7 +1,8 @@
 package utils
 
 import (
-	"github.com/whosonfirst/go-whosonfirst-geojson-v2/properties/geometry"
+       	"github.com/whosonfirst/go-whosonfirst-geojson-v2"
+       	// "github.com/whosonfirst/go-whosonfirst-geojson-v2/properties/geometry"
 	"github.com/whosonfirst/go-whosonfirst-hash"
 )
 
@@ -18,16 +19,11 @@ func HashFeature(f geojson.Feature) (string, error) {
 	return h.HashFromJSON(f.Bytes())
 }
 
-func HashGeometryForFeature(f geojson.Feature) (string, error) {
-
-	geom, err := geometry.ToString(f)
-
-	if err != nil {
-		return "", err
-	}
-
-	return HashGeomtry([]byte(geom))
-}
+// this causes an import loop so we're just going to leave it
+// here as a reference for now... (20170801/thisisaaronland)
+// HashGeometryForFeature(f geojson.Feature) (string, error)
+// geom, err := geometry.ToString(f)
+// return HashGeometry([]byte(geom))
 
 func HashGeometry(geom []byte) (string, error) {
 
@@ -37,5 +33,5 @@ func HashGeometry(geom []byte) (string, error) {
 		return "", err
 	}
 
-	return h.HashFromJSON(f.Bytes())
+	return h.HashFromJSON(geom)
 }
