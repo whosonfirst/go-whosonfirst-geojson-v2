375857a9b0aa62daaf8fe1ab565d01e1a68b8437
thisisaaronland <devnull@localhost>
2018-10-29 10:20:42 -0700

    first pass at support for MultiPoint polygons

--- geometry/polygon.go
+++ geometry/polygon.go
@@ -149,7 +149,6 @@ func PolygonsForGeometry(g *geojson.Geometry) ([]geojson.Polygon, error) {
 			}
 
 			polys = append(polys, polygon)
-
 		}
 
 	case "Point":
@@ -178,7 +177,35 @@ func PolygonsForGeometry(g *geojson.Geometry) ([]geojson.Polygon, error) {
 			Interior: interior,
 		}
 
-		return []geojson.Polygon{polygon}, nil
+		polys = []geojson.Polygon{polygon}
+		return polys, nil
+
+	case "MultiPoint":
+
+		multi_coords := make([]geom.Coord, len(coords))
+
+		for i, c := range coords {
+
+			pt := c.Array()
+
+			lat := pt[1].Float()
+			lon := pt[0].Float()
+
+			coord, _ := utils.NewCoordinateFromLatLons(lat, lon)
+			multi_coords[i] = coord
+		}
+
+		exterior, err := utils.NewPolygonFromCoords(multi_coords)
+
+		if err != nil {
+			return nil, err
+		}
+
+		polygon := Polygon{
+			Exterior: exterior,
+		}
+
+		polys = []geojson.Polygon{polygon}
 
 	default:
 
